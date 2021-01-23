d02cb69bfab91acebea2945b09ef4de1c27ee855
MarinV0321 <marin.lff@libero.it>
2021-01-23 22:04:57 +0200

    CLI parse function and tests

--- test/issues_test.exs
+++ test/issues_test.exs
@@ -5,4 +5,19 @@ defmodule IssuesTest do
   test "greets the world" do
     assert Issues.hello() == :world
   end
+
+  import Issues.CLI, only: [parse_args: 1]
+
+  test "help option selected, --help or -h, :help returned." do
+    assert parse_args(["--help", "anything"]) == :help
+    assert parse_args(["-h", "anything"]) == :help
+  end
+
+  test "Three values given, three returned." do
+    assert parse_args(["user", "project", "99"]) == {"user", "project", 99}
+  end
+
+  test "Only user and project given, count defaulted to 4." do
+    assert parse_args(["user", "project"]) == {"user", "project", 4}
+  end
 end
